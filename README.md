c883a74408ba391d3a681415cec9b61302b727db
Maia <electric.pen@gmail.com>
2022-11-25 16:12:22 -0800

    Updated worksheet branch

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# A repository with solutions and personal whiteboard diagrams for the problems on neetcode.io in javascript
+## A repository with solutions and personal whiteboard diagrams for the problems on neetcode.io in javascript
+
+### How to use this repo:
+- Fork a copy to your own repo
+- Clone your forked copy
+- Navigate into the project root
+- run `npm install`
+  
+### How to test your code:
+- run `npm test -- test/`[testFileName]
+- example `npm test -- test/testRemoveDuplicates.js`
--- remove_duplicates_sorted_array/removeDuplicates.js
+++ remove_duplicates_sorted_array/removeDuplicates.js
@@ -1,62 +1,13 @@
 // Note: This is leetcode problem# 26:
 // https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 // Note 2: The official leetcode problem requires O(1) space, and that you mutate the original array
-// I have provided additional examples of approaches to the problem that do not mutate the original (but take O(n) space)
 
-const removeDuplicatesNaive = (sortedArray) => {
-    for(let index in sortedArray) {
-        const lastElement = index === 0 ? undefined : sortedArray[index -1]
-        const currentElement = sortedArray[index]
+const removeDuplicates = (nums) => {
+    // Your code here
+    // You can test this code by typing the following into your command line from the project directory
+    // npm test -- test/testRemoveDuplicates.js
 
-        if(currentElement === lastElement) {
-            sortedArray.splice(index, 1)
-        }
-    }
-    return sortedArray
-}
-
-const removeDuplicatesNonDestructive = (sortedArray) => {
-    // this method is O(n) space but also does not mutate the input array
-    // the Naive approach both mutates the input array and is O(n^2) time complexity (this is O(n))
-    // the Dangerous speed approach is similar in speed to this approach but mutates the input
-    // therefore this is probably the best overall approach if not allowed to mutate the original
-    
-    const results = []
-
-    for(let index in sortedArray) {
-        const lastResult = results.length === 0 ? undefined : results[results.length -1]
-        const currentElement = sortedArray[index]
-
-        if(currentElement !== lastResult) {
-            results.push(currentElement)
-        }
-    }
-
-    return results
-}
-
-const removeDuplicatesSpeed = (sortedArray) => {
-    let lastUniqueIndex = -1
-    let lastUniqueElement = undefined
-
-    for(let index = 0; index < sortedArray.length; index++) {
-        const currentElement = sortedArray[index]
-        
-        if(currentElement !== lastUniqueElement) {
-            lastUniqueElement = currentElement
-            lastUniqueIndex++
-            sortedArray[lastUniqueIndex] = currentElement
-        }
-    }
-
-    const duplicateCount = sortedArray.length - lastUniqueIndex - 1
-
-    for(let i = 0; i < duplicateCount; i++) {
-        sortedArray.pop()
-    }
-
-    return sortedArray
 }
 
 // Change the export to a different function name if you wish to test a variation
-module.exports = {removeDuplicates: removeDuplicatesSpeed}
+module.exports = {removeDuplicates: removeDuplicates}
