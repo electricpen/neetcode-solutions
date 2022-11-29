e1275a8dd9054fd8b1a40c13849298ea47321de4
Maia <electric.pen@gmail.com>
2022-11-29 15:34:53 -0800

    Added the leetcode link to RemoveElement

--- remove_element/removeElement.js
+++ remove_element/removeElement.js
@@ -1,3 +1,6 @@
+// Leetcode problem #27
+// https://leetcode.com/problems/remove-element/
+
 const removeElement = (nums, val) => {
     // Your code here
     const getLastReplaceable = lastReplaceable => {
