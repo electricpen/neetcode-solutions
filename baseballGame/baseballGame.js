5e383ec59480adc80c94e717a60e657d0726106b
Maia <electric.pen@gmail.com>
2022-11-29 16:44:35 -0800

    Added note about stack datastructure in basebalLGame problem

--- baseballGame/baseballGame.js
+++ baseballGame/baseballGame.js
@@ -1,6 +1,9 @@
 // Leetcode problem #682
 // https://leetcode.com/problems/baseball-game/
 
+// The lesson in this portion is about Stacks. I would highly recommend that you write your own
+// There is a sample stack written in the /dataStructures directory if you do not wish to write one
+
 var calPoints = function(operations) {
     
 };
