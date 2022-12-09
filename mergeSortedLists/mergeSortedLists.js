0ff2c897175fa2b57f20edcdd5cbab0f8b90a7f9
Maia <electric.pen@gmail.com>
2022-12-09 11:35:47 -0800

    Revert "Add stubs for mergeLists and tests"
    This reverts commit f3a8cb02574fa1da57ce5677640fe0c473596f34.

--- mergeSortedLists/mergeSortedLists.js
+++
@@ -1,9 +0,0 @@
-const { LinkedList } = require("../dataStructures/LinkedList")
-
-const mergeLists = (list1, list2) => {
-
-}
-
-module.exports = {
-    mergeLists: mergeLists
-}
--- test/testMergeSortedLists.js
+++
@@ -1,7 +0,0 @@
-const { mergeLists } = require("../mergeSortedLists/mergeSortedLists")
-const assert = require('assert')
-const { describe, it } = require('mocha')
-
-describe("mergeLists", () => {
-    
-})
